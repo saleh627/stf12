51a6605e8d80ded6948a8287f8d3ece4ab559981
saleh627 <saleh627000@gmail.com>
2016-07-02 15:56:30 +0430

    Add files via upload

---
+++ plugins/time.lua
@@ -0,0 +1,10 @@
+﻿function run(msg, matches)
+local url = http.request('http://api.gpmod.ir/time/')
+local jdat = json:decode(url)
+ local text =  '🕒 ساعت '..jdat.FAtime..' \n📆 امروز '..jdat.FAdate..' میباشد.\n    —--\n🕒 '..jdat.ENtime..'\n📆 '..jdat.ENdate.. '\n\n@stf_tem'
+return text
+end
+return {
+  patterns = {"^[#/!]([Tt][iI][Mm][Ee])$"}, 
+run = run, 
+}
